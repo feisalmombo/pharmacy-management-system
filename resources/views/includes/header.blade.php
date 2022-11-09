f87835b611e03851b5b715f74cdca675ddc592da
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-09 17:06:52 +0300

    WIP

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,29 +1,29 @@
 <!-- Header -->
 <div class="header">
-			
+
 	<!-- Logo -->
 	<div class="header-left">
 		<a href="{{route('dashboard')}}" class="logo">
-			<img src="@if(!empty(AppSettings::get('logo'))) {{asset('storage/'.AppSettings::get('logo'))}} @else{{asset('assets/img/logo.png')}} @endif" alt="Logo">
+			<img src="@if(!empty(AppSettings::get('logo'))) {{asset('storage/'.AppSettings::get('logo'))}} @else{{asset('assets/img/logo.png')}} @endif" class="rounded-circle" alt="Logo">
 		</a>
 		<a href="{{route('dashboard')}}" class="logo logo-small">
 			<img src="{{asset('assets/img/logo-small.png')}}" alt="Logo" width="30" height="30">
 		</a>
 	</div>
 	<!-- /Logo -->
-	
+
 	<a href="javascript:void(0);" id="toggle_btn">
 		<i class="fe fe-text-align-left"></i>
 	</a>
-	
-	
-	
+
+
+
 	<!-- Mobile Menu Toggle -->
 	<a class="mobile_btn" id="mobile_btn">
 		<i class="fa fa-bars"></i>
 	</a>
 	<!-- /Mobile Menu Toggle -->
-	
+
 	<!-- Header Right Menu -->
 	<ul class="nav user-menu">
 
@@ -52,13 +52,13 @@
 												<span class="noti-title">{{$notification->data['product_name']}} is only {{$notification->data['quantity']}} left.</span>
 												<span>Please update the purchase quantity </span>
 											</p>
-											
+
 											<p class="noti-time"><span class="notification-time">{{$notification->created_at->diffForHumans()}}</span></p>
 										</div>
 									</div>
 								</a>
 							</li>
-						@endforeach						
+						@endforeach
 					</ul>
 				</div>
 				<div class="topnav-dropdown-footer">
@@ -67,7 +67,7 @@
 			</div>
 		</li>
 		<!-- /Notifications -->
-		
+
 		<!-- User Menu -->
 		<li class="nav-item dropdown has-arrow">
 			<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
@@ -82,7 +82,7 @@
 						<h6>{{auth()->user()->name}}</h6>
 					</div>
 				</div>
-				
+
 				<a class="dropdown-item" href="{{route('profile')}}">My Profile</a>
 				@can('view-settings')<a class="dropdown-item" href="{{route('settings')}}">Settings</a>@endcan
 				@can('backup-app')
@@ -95,9 +95,9 @@
 			</div>
 		</li>
 		<!-- /User Menu -->
-		
+
 	</ul>
 	<!-- /Header Right Menu -->
-	
+
 </div>
 <!-- /Header -->
