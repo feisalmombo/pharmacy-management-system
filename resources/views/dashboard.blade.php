ce0a2e5a0804af916a82d8573d281352c3616b7a
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-10 16:37:58 +0300

    WIP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -116,7 +116,7 @@
 				</div>
 				<div class="card-body">
 					<div class="table-responsive">
-						<table class="table table-hover table-center mb-0">
+						<table class="datatable table table-hover table-center mb-0">
 							<thead>
 								<tr>
 									<th>Medicine</th>
