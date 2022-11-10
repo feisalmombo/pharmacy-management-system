ae1282565a218ed3d381435853d9a6826f555753
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-10 15:41:39 +0300

    WIP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,24 +16,26 @@
 
 	<div class="row">
 		<div class="col-xl-3 col-sm-6 col-12">
-			<div class="card">
-				<div class="card-body">
-					<div class="dash-widget-header">
-						<span class="dash-widget-icon text-primary border-primary">
-							<i class="fe fe-money"></i>
-						</span>
-						<div class="dash-count">
-							<h3>{{AppSettings::get('app_currency', '$')}} {{$today_sales}}</h3>
-						</div>
-					</div>
-					<div class="dash-widget-info">
-						<h6 class="text-muted">Today Sales Cash</h6>
-						<div class="progress progress-sm">
-							<div class="progress-bar bg-primary w-50"></div>
-						</div>
-					</div>
-				</div>
-			</div>
+            <a href="">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="dash-widget-header">
+                            <span class="dash-widget-icon text-primary border-primary">
+                                <i class="fe fe-money"></i>
+                            </span>
+                            <div class="dash-count">
+                                <h3>{{AppSettings::get('app_currency', '$')}} {{$today_sales}}</h3>
+                            </div>
+                        </div>
+                        <div class="dash-widget-info">
+                            <h6 class="text-muted">Today Sales Cash</h6>
+                            <div class="progress progress-sm">
+                                <div class="progress-bar bg-primary w-50"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
 		</div>
 		<div class="col-xl-3 col-sm-6 col-12">
 			<div class="card">
