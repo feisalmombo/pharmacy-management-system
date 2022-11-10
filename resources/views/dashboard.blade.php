1a76144e53ee43bbb68edb2ea857b925bb224fbf
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-10 15:44:31 +0300

    WIP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -61,25 +61,27 @@
             </a>
 		</div>
 		<div class="col-xl-3 col-sm-6 col-12">
-			<div class="card">
-				<div class="card-body">
-					<div class="dash-widget-header">
-						<span class="dash-widget-icon text-danger border-danger">
-							<i class="fe fe-folder"></i>
-						</span>
-						<div class="dash-count">
-							<h3>{{$total_expired_products}}</h3>
-						</div>
-					</div>
-					<div class="dash-widget-info">
+            <a href="#">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="dash-widget-header">
+                            <span class="dash-widget-icon text-danger border-danger">
+                                <i class="fe fe-folder"></i>
+                            </span>
+                            <div class="dash-count">
+                                <h3>{{$total_expired_products}}</h3>
+                            </div>
+                        </div>
+                        <div class="dash-widget-info">
 
-						<h6 class="text-muted">Expired Products</h6>
-						<div class="progress progress-sm">
-							<div class="progress-bar bg-danger w-50"></div>
-						</div>
-					</div>
-				</div>
-			</div>
+                            <h6 class="text-muted">Expired Products</h6>
+                            <div class="progress progress-sm">
+                                <div class="progress-bar bg-danger w-50"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
 		</div>
 		<div class="col-xl-3 col-sm-6 col-12">
 			<div class="card">
