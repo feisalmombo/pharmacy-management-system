63fa561c7fe87f27da0086906c05a2abb4697238
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-12 12:41:41 +0300

    WIP

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <h1>Login</h1>
-<p class="account-subtitle">Access to our dashboard</p>
+{{--  <p class="account-subtitle">Access to our dashboard</p>  --}}
 @if (session('login_error'))
 <x-alerts.danger :error="session('login_error')" />
 @endif
