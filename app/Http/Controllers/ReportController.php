ebebd6177072ae9bfeb486c8d6b88ddd0054e763
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-14 12:56:07 +0300

    WIP

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -13,17 +13,20 @@ class ReportController extends Controller
 {
     public function index(){
         $title = "generate Reports";
+
         return view('reports',compact(
             'title',
         ));
     }
 
     public function getData(Request $request){
+
         $this->validate($request,[
             'from_date'=>'required',
             'to_date'=>'required',
             'resource'=>'required',
         ]);
+
         $from_date = $request->from_date;
         $to_date = $request->to_date;
         if ($request->resource == 'sales'){
@@ -31,16 +34,19 @@ class ReportController extends Controller
             $total_sales = $sales->count();
             $total_cash =$sales->sum('total_price');
             $title = "Sales Reports";
+
             return view('reports',compact('sales','title','total_sales','total_cash'));
         }
         if($request->resource == "products"){
             $title = "Products Reports";
             $products = Product::whereBetween(DB::raw('DATE(created_at)'), array($from_date, $to_date))->get();
+
             return view('reports',compact('title','products'));
         }
         if($request->resource == 'purchases'){
             $title = "Purchases Reports";
             $purchases = Purchase::whereBetween(DB::raw('DATE(created_at)'), array($from_date, $to_date))->get();
+
             return view('reports',compact('title','purchases'));
         }
     }
