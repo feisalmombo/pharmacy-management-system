006ecc0f2d1ad6a92a990bce472e6dd61f7307e1
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-15 17:17:55 +0300

    fix minor bugs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <h1>Login</h1>
-{{--  <p class="account-subtitle">Access to our dashboard</p>  --}}
 @if (session('login_error'))
 <x-alerts.danger :error="session('login_error')" />
 @endif
